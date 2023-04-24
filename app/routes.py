d6e722b0a878e69bd8e6cf37444c9d0f5ba8275d
Stacey <stacey.l.joseph@gmail.com>
2023-04-24 15:02:45 -0400

    adds validate book helper function and handles 400 & 404 errors

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -30,16 +30,20 @@ def handle_books():
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def handle_book(book_id):
+    book = validate_book(book_id)
+    
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
+
+def validate_book(book_id):
     try: 
         book_id = int(book_id)
-    except: 
-        return {"message":f"book id {book_id} invalid"}, 400
-    
+    except:
+        abort(make_response({"message": f"book {book_id} invalid"}, 400))
     for book in books:
         if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
-            }
-    return {"message":f"book {book_id} not found"}, 404
+            return book
+    abort(make_response({"message" : f"book {book_id} not found"}, 404))
