dbe4d8455104248b4ed632dac7e48afa5ca2d5e3
Stacey <stacey.l.joseph@gmail.com>
2023-05-02 00:17:15 -0400

    defining routes that read model records

--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,95 @@
+# from os import abort
+
+# import modules
+# db is our connection to the database
 from app import db
+
+# importing the Book model
 from app.models.book import Book
-from flask import Blueprint, jsonify, make_response, request
 
-books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+# import dependencies
+from flask import Blueprint, jsonify, abort, make_response, request
 
-@books_bp.route("", methods=["POST"])
-def create_book():
-    request_body = request.get_json()
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
 
-    db.session.add(new_book)
-    db.session.commit()
+# creating the Blueprint instance to group all routes that 
+# start with /books
+# "books" is the debugging name for this Blueprint
+# __name__ provides information the blueprint uses for certain aspects of routing
+# url_prefix="/books" is a keyword argument that indicates that every endpoint 
+# using this Blueprint should be treated like it starts with /books.
+# books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-    return make_response(f"Book {new_book.title} successfully created", 201)
+# original 
+# decorator that uses the books_bp Blueprint to define an endpoint 
+# and accepted HTTP method. 
+# @books_bp.route("", methods=["POST"])
+# The function after the decorator will execute whenever a 
+# matching HTTP request is received.
+# def handle_books():
+    
+#     # request_body variable holds the body contents of the HTTP request 
+#     # in a Python data structure
+#     request_body = request.get_json()
+    
+#     # error checking/ input validation
+#     # fixes 500 internal error for invalid request
+#     if "title" not in request_body or "description" not in request_body:
+#         return make_response("Invalid request", 400)
+    
+#     # creates a variable to hold the new instance of Book using the data in request_body. 
+#     # title = & description = are keyword arguments that match our model attributes, 
+#     # and access the request_body values to create the Book instance
+#     new_book = Book(title=request_body["title"],
+#                     description=request_body["description"])
 
-@books_bp.route("", methods=["GET"])
-def read_all_books():
-    books_response = []
-    books = Book.query.all()
-    for book in books:
-        books_response.append(
-            {
+#     # the database's way of collecting changes(add(new_book)) that need to be made
+#     db.session.add(new_book)
+    
+#     # database to save and commit the collected changes.
+#     db.session.commit()
+
+#     # explicitly return the HTTP response for the endpoint
+#     # make_response instantiates a Response object
+#     # (parameter) is the HTTP response body as a string, 
+#     # unless we have more specific requirements
+#     # 201 response code, 200 is the default
+#     return make_response(f"Book {new_book.title} successfully created", 201)
+    
+#     # can also return the response implicitly as a tuple:
+#     # and return default 200 status code
+#     # return f"Book {new_book.title} successfully created"
+
+
+# refactor
+@books_bp.route("", methods=["GET", "POST"])
+def handle_books():
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
                 "id": book.id,
                 "title": book.title,
                 "description": book.description
-            }
-        )
-    return jsonify(books_response)
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":        
+        request_body = request.get_json()
+        
+        if "title" not in request_body or "description" not in request_body:
+            return make_response("Invalid request", 400)
+        
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
+
+        db.session.add(new_book)
+        
+        db.session.commit()
+
+        return make_response(f"Book {new_book.title} successfully created", 201)
+        
+
 
 #def validate_book(book_id):
 #    try:
@@ -41,6 +103,20 @@ def read_all_books():
 #
 #    abort(make_response({"message":f"book {book_id} not found"}, 404))
         
+
+# @books_bp.route("", methods=["GET"])
+# def handle_books():
+#     books_response = []
+#     for book in books:
+#         books_response.append(
+#             {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description
+#             }
+#         )
+#     return jsonify(books_response)
+
 # @books_bp.route("/<book_id>", methods=["GET"])
 # def handle_book(book_id):
 #     book = validate_book(book_id)
