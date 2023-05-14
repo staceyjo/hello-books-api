eb5940568fa0ddc935e4df5cd7938b1cb9f0b3a1
Stacey <stacey.l.joseph@gmail.com>
2023-05-14 15:13:55 -0400

    updating tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -63,52 +63,119 @@ def test_to_dict_missing_description():
     assert result["description"] is None
     
     
-# def test_from_dict_returns_book():
-#     # Arrange
-#     book_data = {
-#         "title": "New Book",
-#         "description": "The Best!"
-#     }
+def test_from_dict_returns_book():
+    # Arrange
+    book_data = {
+        "title": "New Book",
+        "description": "The Best!"
+    }
 
-#     # Act
-#     new_book = Book.from_dict(book_data)
+    # Act
+    new_book = Book.from_dict(book_data)
 
-#     # Assert
-#     assert new_book.title == "New Book"
-#     assert new_book.description == "The Best!"
+    # Assert
+    assert new_book.title == "New Book"
+    assert new_book.description == "The Best!"
     
-# def test_from_dict_with_no_title():
-#     # Arrange
-#     book_data = {
-#         "description": "The Best!"
-#     }
+def test_from_dict_with_no_title():
+    # Arrange
+    book_data = {
+        "description": "The Best!"
+    }
 
-#     # Act & Assert
-#     with pytest.raises(KeyError, match = 'title'):
-#         new_book = Book.from_dict(book_data)
+    # Act & Assert
+    with pytest.raises(KeyError, match = 'title'):
+        new_book = Book.from_dict(book_data)
 
-# def test_from_dict_with_no_description():
-#     # Arrange
-#     book_data = {
-#         "title": "New Book"
-#     }
+def test_from_dict_with_no_description():
+    # Arrange
+    book_data = {
+        "title": "New Book"
+    }
 
-#     # Act & Assert
-#     with pytest.raises(KeyError, match = 'description'):
-#         new_book = Book.from_dict(book_data)
+    # Act & Assert
+    with pytest.raises(KeyError, match = 'description'):
+        new_book = Book.from_dict(book_data)
         
-# def test_from_dict_with_extra_keys():
-#     # Arrange
-#     book_data = {
-#         "extra": "some stuff",
-#         "title": "New Book",
-#         "description": "The Best!",
-#         "another": "last value"
-#     }
+def test_from_dict_with_extra_keys():
+    # Arrange
+    book_data = {
+        "extra": "some stuff",
+        "title": "New Book",
+        "description": "The Best!",
+        "another": "last value"
+    }
     
-#     # Act
-#     new_book = Book.from_dict(book_data)
+    # Act
+    new_book = Book.from_dict(book_data)
 
-#     # Assert
-#     assert new_book.title == "New Book"
-#     assert new_book.description == "The Best!"
+    # Assert
+    assert new_book.title == "New Book"
+    assert new_book.description == "The Best!"
+
+# When we have records, `read_all_books` returns a list containing a dictionary representing each `Book`
+def test_get_all_books_with_two_records(client, two_saved_books):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body[0] == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
+    assert response_body[1] == {
+        "id": 2,
+        "title": "Mountain Book",
+        "description": "i luv 2 climb rocks"
+    }
+
+# When we have records and a `title` query in the request arguments, `read_all_books` returns a list containing only the `Book`s which match the query
+def test_get_all_books_with_title_query_matching_none(client, two_saved_books):
+    # Act
+    data = {'title': 'Desert Book'}
+    response = client.get("/books", query_string = data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+# When we have records and a `title` query in the request arguments, `read_all_books` returns a list containing only the `Book`s which match the query
+def test_get_all_books_with_title_query_matching_one(client, two_saved_books):
+    # Act
+    data = {'title': 'Ocean Book'}
+    response = client.get("/books", query_string = data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 1
+    assert response_body[0] == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
+
+# When we call `read_one_book` with a numeric ID that doesn't have a record, we get the expected error message
+def test_get_one_book_id_not_found(client, two_saved_books):
+    # Act
+    response = client.get("/books/3")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {"message":"book 3 not found"}
+
+# When we call `read_one_book` with a non-numeric ID, we get the expected error message
+def test_get_one_book_id_invalid(client, two_saved_books):
+    # Act
+    response = client.get("/books/cat")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert response_body == {"message":"book cat invalid"}
